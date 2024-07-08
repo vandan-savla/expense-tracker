c280504df9e8587a43e538edb8be856400d1a594
vandan savla <vdsavla@gmail.com>
2024-07-08 20:14:05 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import Dashboard from './Components/Dashboard/Dashboard';
+
+import Signin from './Components/Auth/Signin';
+import Signup from './Components/Auth/Signup';
+// import AuthPage from './Components/Auth/AuthPage';
 
 function App() {
+
+
   return (
-    <div>App</div>
+    <div>
+
+      <Routes>
+        
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+    </div>
   )
 }
 
